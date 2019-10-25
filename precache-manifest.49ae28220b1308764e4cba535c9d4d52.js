e90ea710e6e13227532b9e64d040bbe3e27e82b4
michiharu <michi1diver@gmail.com>
2019-10-25 20:39:01 +0900

    Updates

---
+++ precache-manifest.49ae28220b1308764e4cba535c9d4d52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4324f9b42d706d570787d67e9f0728",
+    "url": "/Kuvakirja/index.html"
+  },
+  {
+    "revision": "ed7e4c96a63eee9c8ac2",
+    "url": "/Kuvakirja/static/js/2.66dd19a7.chunk.js"
+  },
+  {
+    "revision": "c5550cbadf7f3a4cfc79",
+    "url": "/Kuvakirja/static/js/main.5758c066.chunk.js"
+  },
+  {
+    "revision": "e9fa16222c0ffd21c065",
+    "url": "/Kuvakirja/static/js/runtime-main.8568cd40.js"
+  }
+]);
